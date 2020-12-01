b67de154d5c1bcb571b6953784be40648975c4c5
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-01 17:50:42 -0500

    Create constructor with necessary properties

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,10 @@
 class Card {
-
+  constructor(number, question, answers, correctAnswer) {
+    this.number = number;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
 }
 
 module.exports = Card;
