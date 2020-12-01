b43489e98afef985a4cabbf97e54f776eb1cd4d8
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-01 18:19:30 -0500

    Pseudocode test

---
+++ test/Turn-test.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Card');
+
+describe('Turn', function() {
+
+  it.skip('should be a function', () => {
+    //does it exist?
+  })
+
+  it.skip('should be an instance of Card', () => {
+    //does it exist?
+  })
+
+  it.skip('should store the user\'s guess', () => {
+    //does it contain a property for the user\'s guess?
+  })
+
+  it.skip('should have current card as an object instance of Card', () => {
+    //is the object instance contain the correct data? is it deep.equal?
+  })
+
+  it.skip('should return a guess', () => {
+    //does method exist
+    //does it return what's needed
+  })
+
+  it.skip('should return a card', () => {
+    //does method exists
+    //does it return a card
+  })
+
+  it.skip('should evaluate if user\'s guess matches card\'s correct answer', () => {
+    //does method exist
+    //does it return true if the answer is correct?
+    //does it return false if the answer is incorrect?
+  })
+
+  it.skip('should give user feedback based on their answer', () => {
+    //does method exist
+    //if evaluated to true, feedback should be correct
+    //if evaluated to false, feedback should be incorrect
+
+  })
+})
