3d5612a7c4460750b8946e2cf9f6afc1c34b1eba
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-02 17:18:38 -0500

    Create tests to check for constructor properties

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,14 +6,14 @@ const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe('Round', function() {
-  let round;
+  let card1, card2, card3, card4, deck, round;
   beforeEach(() => {
-    const card1 = new Card(3, 'What\'s the fastest animal on earth?', ['cheeta', 'sail fish', 'peregrine falcon'], 'peregrine falcon');
-    const card2 = new Card(8, 'What bird did Benjamin Franklin originally suggest to be the US\'s national symbol?', ['dove', 'ruffed grouse', 'wild turkey'], 'wild turkey');
-    const card3 = new Card(10, 'What bird was woody the woodpecker modeled after?', ['pileated woodpecker', 'red-headed woodpecker', 'hairy woodpecker'], 'pileated woodpecker');
-    const card4 = new Card(44, 'What owl is known to say "Who cooks for you"?', ['short eared owl', 'barred owl', 'great horned owl'], 'barred owl');
+    card1 = new Card(3, 'What\'s the fastest animal on earth?', ['cheeta', 'sail fish', 'peregrine falcon'], 'peregrine falcon');
+    card2 = new Card(8, 'What bird did Benjamin Franklin originally suggest to be the US\'s national symbol?', ['dove', 'ruffed grouse', 'wild turkey'], 'wild turkey');
+    card3 = new Card(10, 'What bird was woody the woodpecker modeled after?', ['pileated woodpecker', 'red-headed woodpecker', 'hairy woodpecker'], 'pileated woodpecker');
+    card4 = new Card(44, 'What owl is known to say "Who cooks for you"?', ['short eared owl', 'barred owl', 'great horned owl'], 'barred owl');
     const cards = [card1, card2, card3, card4];
-    const deck = new Deck(cards);
+    deck = new Deck(cards);
     round = new Round(deck);
   })
 
@@ -22,29 +22,34 @@ describe('Round', function() {
     expect(round.deck.cards).to.have.lengthOf.above(1);
   });
 
-  it.skip('should return current card in play', () => {
-
-    //is returnCurrentCard a method
-    //does it return an object
-    //is that object equal to an object in cards array of deck
+  it('should have first card in deck as current card when instantiated', () => {
+    expect(round.currentCard).to.deep.equal(card1);
   });
 
-
-  it.skip('should create new instance of turn when guess is made', () => {
-    //does takeTurn create a new Turn
-    //try more than once
-    //combined top & bottom
+  it('should return first card in deck as current card', () => {
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
   });
 
-  it.skip('should have current card as the first card in the deck when turn count is 0', () => {
-    //??? needed?
-    //does currentCard deep equal desk.cards[0]?
+  it('should keep a turn count', () => {
+    expect(round.turnCount).to.equal(0);
   });
 
-  it.skip('should update turn count', () => {
-    //does turn count update x amount of times when takeTurn is called
-    //try multiple times with expects following
+  it('should have a place to hold incorrect Guesses', () => {
+    expect(round.incorrectGuesses).to.be.an('array');
   });
+
+  it('should update turn count when a user takes a turn', () => {
+    round.takeTurn();
+    expect(round.turnCount).to.equal(1);
+    round.takeTurn();
+    expect(round.turnCount).to.equal(2);
+  })
+
+  it('should create new instance of turn when guess is made', () => {
+    const falcon = round.takeTurn('peregrine falcon');
+    expect(falcon).to.be.an.instanceOf(Turn);
+  });
+
 
   it.skip('should update current card to reflect next card in deck', () => {
     //when turn happens, does the current card match the next card in the deck.cards array?
