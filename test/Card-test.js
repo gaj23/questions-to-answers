daecfaad07737c18846b87f1f8e669fb8afbe09f
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-02 09:08:22 -0500

    Utilize arrow functions

--- test/Card-test.js
+++ test/Card-test.js
@@ -9,23 +9,23 @@ describe('Card', function() {
     card = new Card(8, 'What bird did Benjamin Franklin originally suggest to be the US\'s national symbol?', ['dove', 'ruffed grouse', 'wild turkey'], 'wild turkey');
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Card).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
+  it('should be an instance of Card', () => {
     expect(card).to.be.an.instanceof(Card);
   });
 
-  it('should store a question', function() {
+  it('should store a question', () => {
     expect(card.question).to.equal('What bird did Benjamin Franklin originally suggest to be the US\'s national symbol?');
   });
 
-  it('should store a list of possible answers', function() {
+  it('should store a list of possible answers', () => {
     expect(card.answers).to.deep.equal(['dove', 'ruffed grouse', 'wild turkey']);
   });
 
-  it('should store the correct answer', function() {
+  it('should store the correct answer', () => {
     expect(card.correctAnswer).to.equal('wild turkey');
   });
 });
