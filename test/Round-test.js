2cd4fe75d887e1efe6f5d9c5731e26d7dee47bd0
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-03 20:39:39 -0500

    Remove unnecessary variable

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe('Round', function() {
